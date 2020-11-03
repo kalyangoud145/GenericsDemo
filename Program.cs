42bdac7ff937c55501b31e18e7b90fa29c5f6728
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-03 10:43:06 +0530

    [Kalyan] implementd using generic methods

--- Program.cs
+++ Program.cs
@@ -10,36 +10,18 @@ namespace GenericsDemo
             int[] intArray = { 1, 2, 3, 4, 5 };
             double[] doubleArray = { 1.1, 2.3, 3.6, 4.8, 5.10, 6.369 };
             char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
-            Program.ToPrint(intArray);
-            Program.ToPrint(doubleArray);
-            Program.ToPrint(charArray);
+            Program.ToPrint<int>(intArray);
+            Program.ToPrint<double>(doubleArray);
+            Program.ToPrint<char>(charArray);
         }
 
-        public static void ToPrint(int[] inputArray)
+        public static void ToPrint<T>(T[] inputArray)
         {
-            foreach(int element in inputArray)
+            foreach(var element in inputArray)
             {
                 Console.WriteLine(element);
             }
             Console.WriteLine("**************");
-        }
-
-        public static void ToPrint(double[] inputArray)
-        {
-            foreach(double element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("***********");
-        }
-
-        public static void ToPrint(char[] inputArray)
-        {
-            foreach (char element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("***********");
         }
     }
 }
