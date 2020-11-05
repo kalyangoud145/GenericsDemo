b4837da64e74123b56a4a88990dcfca4fc961b87
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-05 08:26:19 +0530

    [Kalyan] printed datatypes arrays using genric class

---
+++ GenericClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDemo
+{
+    class GenericClass
+    {
+        //Generic class
+        public class PrintArray<T>
+        {
+            private T[] inputArray;
+
+            public PrintArray(T[] inputArray)
+            {
+                this.inputArray = inputArray;
+            }
+            public void ToPrint()
+            {
+                foreach (var element in inputArray)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("**************");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,22 +6,13 @@ namespace GenericsDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             int[] intArray = { 1, 2, 3, 4, 5 };
             double[] doubleArray = { 1.1, 2.3, 3.6, 4.8, 5.10, 6.369 };
             char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
-            Program.ToPrint<int>(intArray);
-            Program.ToPrint<double>(doubleArray);
-            Program.ToPrint<char>(charArray);
+            new GenericClass.PrintArray<int>(intArray).ToPrint();
+            new GenericClass.PrintArray<double>(doubleArray).ToPrint();
+            new GenericClass.PrintArray<char>(charArray).ToPrint();
         }
-
-        public static void ToPrint<T>(T[] inputArray)
-        {
-            foreach(var element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("**************");
-        }
+        
     }
 }
