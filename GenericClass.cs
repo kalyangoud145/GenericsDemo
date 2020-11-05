b7d5b127469c42aee29ac5ac2a0d086242799ffc
kalyangoud145 <65416828+kalyangoud145@users.noreply.github.com>
2020-11-05 08:40:29 +0530

    Update GenericClass.cs

--- GenericClass.cs
+++ GenericClass.cs
@@ -10,11 +10,12 @@ namespace GenericsDemo
         public class PrintArray<T>
         {
             private T[] inputArray;
-
+            //Contructor
             public PrintArray(T[] inputArray)
             {
                 this.inputArray = inputArray;
             }
+            //Method prints elements in the array
             public void ToPrint()
             {
                 foreach (var element in inputArray)
